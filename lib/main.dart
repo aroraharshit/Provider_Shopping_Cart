241e8c5af6b428a5deb88d5543eea9201349bb50
Harshit Arora <86155436+aroraharshit@users.noreply.github.com>
2023-10-02 20:13:25 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_shopping_cart/cart_provider.dart';
+import 'package:provider_shopping_cart/product_list.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => CartProvider(),
+      child: Builder(builder: (BuildContext context) {
+        return MaterialApp(
+          title: 'Flutter Demo',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: ProductListScreen(),
+        );
+      }),
+    );
+  }
+}
